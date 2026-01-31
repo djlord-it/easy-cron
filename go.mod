eb0df45d551e49cf9ab8d3e0df7e363dc6aa16b3
Dushime Jessee Lord <dushime.jessee_lord@courrier.uqam.ca>
2026-01-31 03:01:01 -0500

    Add Redis analytics sink
    Writes time-bucketed counts to Redis with TTL.
    Keys: p:{project}:j:{job}:{type}:{bucket}

--- go.mod
+++ go.mod
@@ -4,4 +4,12 @@ go 1.24.4
 
 require github.com/google/uuid v1.6.0
 
-require github.com/robfig/cron/v3 v3.0.1
+require (
+	github.com/redis/go-redis/v9 v9.17.3
+	github.com/robfig/cron/v3 v3.0.1
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+)
---
+++ internal/analytics/redis.go
@@ -0,0 +1,58 @@
+package analytics
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+
+	"easycron/internal/domain"
+)
+
+type RedisSink struct {
+	client *redis.Client
+}
+
+func NewRedisSink(client *redis.Client) *RedisSink {
+	return &RedisSink{client: client}
+}
+
+func (s *RedisSink) Write(ctx context.Context, event domain.TriggerEvent, config domain.AnalyticsConfig) error {
+	if !config.Enabled {
+		return nil
+	}
+
+	key := buildKey(event.ProjectID.String(), event.JobID.String(), config.Type, event.ScheduledAt, config.Window)
+
+	pipe := s.client.Pipeline()
+	pipe.Incr(ctx, key)
+	pipe.Expire(ctx, key, config.Retention)
+
+	_, err := pipe.Exec(ctx)
+	if err != nil {
+		return fmt.Errorf("redis pipeline: %w", err)
+	}
+
+	return nil
+}
+
+func buildKey(projectID, jobID string, typ domain.AnalyticsType, t time.Time, window time.Duration) string {
+	bucket := truncateToBucket(t, window)
+	return fmt.Sprintf("p:%s:j:%s:%s:%s", projectID, jobID, typ, bucket)
+}
+
+func truncateToBucket(t time.Time, window time.Duration) string {
+	t = t.UTC()
+	switch window {
+	case time.Minute:
+		return t.Format("200601021504")
+	case 5 * time.Minute:
+		minute := (t.Minute() / 5) * 5
+		return t.Format("2006010215") + fmt.Sprintf("%02d", minute)
+	case time.Hour:
+		return t.Format("2006010215")
+	default:
+		return t.Format("200601021504")
+	}
+}
